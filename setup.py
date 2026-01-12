a152148275e54cf995cd5725333ed875fb574580
Aliénor Lahlou <alienor.mz@gmail.com>
2026-01-12 14:07:45 +0100

    incompatible for python 3.7

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
     python_requires=">=3.7",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
