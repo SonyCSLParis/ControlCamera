2d5a7ac4239f44b43310f322e03e05d9f373f70d
Aliénor Lahlou <alienor.mz@gmail.com>
2026-01-12 14:01:40 +0100

    test python version compatibility

--- setup.py
+++ setup.py
@@ -13,12 +13,22 @@ install_requires = [
 ]
 
 
-setup(name="ControlCamera",
-version="0.0.1",
-description="A class to control cameras interfaced with Micro-Manager",
-author="Alienor Lahlou",
-author_email="alienor.lahlou@sony.com",
-packages = find_packages(),
-install_requires = install_requires,
-license="GPLv3",
+setup(
+    name="ControlCamera",
+    version="0.0.1",
+    description="A class to control cameras interfaced with Micro-Manager",
+    author="Alienor Lahlou",
+    author_email="alienor.lahlou@sony.com",
+    packages=find_packages(),
+    install_requires=install_requires,
+    license="GPLv3",
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
 )
